c687221b533c664b3f075f814bf0ba8c09b1ac5f
NIHAL DAS <nihaldas8888@gmail.com>
2025-12-24 15:21:50 +0530

    new name E-commerce Laravel project

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,11 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         @vite(['resources/js/app.ts'])
+
         {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> --}}
 
-         <title>{{ $heading ?? 'E-Commerce' }}</title>
+         <title>{{ $heading ?? 'E-commerce' }}</title>
     </head>
     <body>
         
