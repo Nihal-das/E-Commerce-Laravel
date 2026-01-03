bcf31e1c2bf5efa366c831e4f5fa662c7e300d6a
NIHAL DAS <nihaldas8888@gmail.com>
2026-01-03 12:21:03 +0530

    added cart in mobile v2

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -123,13 +123,16 @@
                                         <a href="/uploads" class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:text-white focus:outline-hidden">upload file</a>
                                         <a href="/admin" class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:text-white focus:outline-hidden">Dashboard</a>
                                     @endif
-
-                                    <a href="{{ route('cart.show') }}" class="z-52 {{ request()->is('/cart') ? 'px-4 py-2 -mt-2 bg-black rounded-2xl' : ''}}">My Cart</a>
+                                    
+                                    <div class="flex justify-between">
+                            
+                                    <a href="{{ route('cart.show') }}" class="text-white {{ request()->is('/cart') ? 'px-4 py-2 -mt-2 bg-black rounded-2xl' : ''}}">My Cart</a>
                                     @if (! $cartCount == 0)
-                                        <div class="absolute top-4 right-22 flex items-center justify-center w-6 h-6 bg-red-700 text-white text-xs font-bold rounded-full border border-amber-50">
+                                        <div class="top-40 right-22 flex items-center justify-center w-6 h-6 bg-red-700 text-white text-xs font-bold rounded-full border border-amber-50">
                                     {{ $cartCount }}
                                     </div>
                                     @endif
+                                    </div>
                                    
                                 </div>
                                 <div class="py-6">
